31ad63ebab2ac796231cbffe25210a70f2f9aac7
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-03-13 22:19:58 +0200

    dashboard-ros connection

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\dashboardController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('test',  [dashboardController::class, 'test']);
+Route::post('position_test',  [dashboardController::class, 'position_test']);
+Route::post('velocity_test',  [dashboardController::class, 'velocity_test']);
+Route::post('PID_test',  [dashboardController::class, 'PID_test']);
+Route::get('Get_IP',  [dashboardController::class, 'Get_IP']);
